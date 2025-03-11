616adf8508d2e221b391e20e98552d0eb1a66464
Andrii Hrushetskyi <hrushetskyi.andrii@gmail.com>
2025-03-11 22:07:44 +0100

    implemented for static coords

--- main.ts
+++ main.ts
@@ -1,134 +1,70 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from "obsidian";
+const SunCalc = require("suncalc");
 
-// Remember to rename these classes and interfaces!
+const tempCoords = {
+	latitude: 49.84079,
+	longitude: 18.29088,
+};
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class EasyThemeSwitcherPlugin extends Plugin {
+	private timeout: ReturnType<typeof setTimeout> | null;
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.checkAndSwitchTheme(tempCoords.latitude, tempCoords.longitude);
 	}
 
 	onunload() {
-
+		if (this.timeout !== null) {
+			clearTimeout(this.timeout);
+			this.timeout = null;
+		}
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	checkAndSwitchTheme(latt: number, long: number) {
+		const now = Date.now();
+		const { dawn, dusk } = SunCalc.getTimes(new Date(), latt, long);
+		const tomorrowDawn = SunCalc.getTimes(
+			new Date(now + 24 * 60 * 60 * 1000),
+			latt,
+			long
+		).dawn;
+
+		const times = {
+			dawn: dawn.getTime(),
+			dusk: dusk.getTime(),
+			tomorrowDawn: tomorrowDawn.getTime(),
+		};
+
+		let checkDelay: number;
+
+		if (now < dawn) {
+			this.setMode("dark");
+			checkDelay = times.dawn - now;
+		} else if (now >= dawn && now < dusk) {
+			this.setMode("light");
+			checkDelay = times.dusk - now;
+		} else {
+			this.setMode("dark");
+			checkDelay = times.tomorrowDawn - now;
+		}
+
+		this.timeout = setTimeout(
+			this.checkAndSwitchTheme.bind(this, latt, long),
+			checkDelay
+		);
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	setMode(targetMode: "dark" | "light") {
+		switch (targetMode) {
+			case "dark":
+				document.body.classList.remove("theme-light");
+				document.body.classList.add("theme-dark");
+				break;
+			case "light":
+				document.body.classList.remove("theme-dark");
+				document.body.classList.add("theme-light");
+				break;
+		}
+		this.app.workspace.trigger("css-change");
 	}
 }
